2b2e12ae512bc094c2b459b7e38e9847cb00c588
Felamande <elweinfancon@gmail.com>
2015-09-16 22:29:49 +0800

    Incomplete ftp implementation.

---
+++ uri/ftp.go
@@ -0,0 +1,18 @@
+package uri
+
+import (
+	"net"
+)
+
+type UriFtp struct {
+	conn net.Conn
+	scheme string
+	path   string
+	host   string
+}
+
+func (u *UriFtp) Scheme() string {
+	return "ftp"
+}
+
+func(u *UriFtp)
