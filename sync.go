e31645dee75d6c22c9976f5782547cf9fafd063c
Felamande <elweinfancon@gmail.com>
2015-09-17 12:59:58 +0800

    Add primitive web api support.

--- sync.go
+++ sync.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/Felamande/filesync/log"
 	"github.com/go-martini/martini"
+
+	"os/signal"
 
 	"github.com/Felamande/filesync/syncer"
 )
@@ -13,6 +19,11 @@ func main() {
 	s := syncer.New()
 	m := martini.Classic()
 	l := log.NewFileLogger("./.log/http.log")
+
+	SigChan := make(chan os.Signal, 2)
+	signal.Notify(SigChan, os.Interrupt, os.Kill)
+	go handleSignal(SigChan, s)
+
 	m.Map(s)
 	m.Map(l)
 	m.Post("/new", NewPair)
@@ -22,19 +33,42 @@ func main() {
 
 }
 
-func NewPair(logger *log.FileLogger, s *syncer.Syncer, w http.ResponseWriter, r *http.Request) int {
+func NewPair(logger *log.FileLogger, s *syncer.Syncer, w http.ResponseWriter, r *http.Request) string {
 	r.ParseForm()
-	_, lExist := r.Form["left"]
-	_, rExist := r.Form["right"]
+	lName := r.FormValue("left")
+	rName := r.FormValue("right")
 
-	if !lExist || !rExist {
-		return 400
+	err := s.NewPair(syncer.SyncConfig{false, false, false}, lName, rName)
 
+	if err != nil {
+		return err.Error()
 	}
-	return 200
+
+	return "Success"
 
 }
 
 func HelloNewPair() string {
-	return "Hello"
+	return "<!DOCTYPE html><head><script type='text/javascript' src='http://libs.baidu.com/jquery/2.0.3/jquery.min.js'></script></head><body>Hello</body>"
+}
+
+func handleSignal(c chan os.Signal, s *syncer.Syncer) {
+	for {
+		select {
+		case sig := <-c:
+			fmt.Println(sig.String())
+			c := syncer.SavedConfig{}
+			for _, pair := range s.SyncPairs {
+				fmt.Println(pair.Left.Uri())
+				c.Pairs = append(c.Pairs, syncer.SyncPairConfig{
+					Left:   pair.Left.Uri(),
+					Right:  pair.Right.Uri(),
+					Config: pair.Config,
+				})
+				b, _ := json.Marshal(c.Pairs)
+				ioutil.WriteFile("config.json", b, 0777)
+				os.Exit(0)
+			}
+		}
+	}
 }
