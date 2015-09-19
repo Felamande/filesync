a78f2dc9c4a1198682876c262995cf57fd8ea6ad
Felamande <elweinfancon@gmail.com>
2015-09-19 12:40:44 +0800

    Add struct SavedConfig.

---
+++ syncer/config.go
@@ -0,0 +1,15 @@
+package syncer
+
+type GlobalConfig struct {
+}
+
+type SavedConfig struct {
+	Pairs []SyncPairConfig `json:"pairs"`
+	Port  string           `json:"port"`
+}
+
+type SyncPairConfig struct {
+	Left   string     `json:"left"`
+	Right  string     `json:"right"`
+	Config SyncConfig `json:"config"`
+}
