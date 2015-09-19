f354bc0b6c5b7f050a38444fb7384b9153b592bd
Felamande <elweinfancon@gmail.com>
2015-09-19 12:22:37 +0800

    Implement system service.

--- sync.go
+++ sync.go
@@ -1,50 +1,58 @@
 package main
 
 import (
-	"encoding/json"
+	"flag"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 
-	"github.com/Felamande/filesync/log"
-	"github.com/go-martini/martini"
-
-	"os/signal"
+	svc "github.com/kardianos/service"
 
 	"github.com/Felamande/filesync/syncer"
 )
 
+var run *bool = flag.Bool("run", false, "Run in the shell. -svcctl will be disabled.")
+var controls *string = flag.String("svcctl", "install,start", "value:[start,stop,restart,install,uninstall], can be multiple values separated by commas")
+var help *bool = flag.Bool("help", false, "Get help")
+
 func main() {
-	s := syncer.New()
-	m := martini.Classic()
-	l := log.NewFileLogger("./.log/http.log")
+	flag.Parse()
 
-	SigChan := make(chan os.Signal, 2)
-	signal.Notify(SigChan, os.Interrupt, os.Kill)
-	go handleSignal(SigChan, s)
-
-	m.Map(s)
-	m.Map(l)
-	m.Post("/new", NewPair)
-	m.Get("/new", HelloNewPair)
-	go s.Run()
-	http.ListenAndServe(":20000", m)
-
-}
-
-func NewPair(logger *log.FileLogger, s *syncer.Syncer, w http.ResponseWriter, r *http.Request) string {
-	r.ParseForm()
-	lName := r.FormValue("left")
-	rName := r.FormValue("right")
-
-	err := s.NewPair(syncer.SyncConfig{false, false, false}, lName, rName)
-
-	if err != nil {
-		return err.Error()
+	if len(os.Args) == 1 {
+		flag.Usage()
+		return
 	}
 
-	return "Success"
+	if *help {
+		flag.Usage()
+		return
+	}
+
+	p := &Program{}
+	s, err := svc.New(p, &svc.Config{
+		Name:        "Filesync",
+		DisplayName: "FileSync Service",
+		Description: "Filesync is a simple tool to sync files between multiple directory pairs.",
+		Arguments:   []string{"-run"},
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if *run {
+		err := s.Run()
+		fmt.Println("-run with error: ", err.Error())
+		return
+	}
+
+	Actions := strings.Split(*controls, ",")
+	for _, action := range Actions {
+		err := svc.Control(s, action)
+		fmt.Println(err)
+	}
 
 }
 
@@ -52,23 +60,18 @@ func HelloNewPair() string {
 	return "<!DOCTYPE html><head><script type='text/javascript' src='http://libs.baidu.com/jquery/2.0.3/jquery.min.js'></script></head><body>Hello</body>"
 }
 
-func handleSignal(c chan os.Signal, s *syncer.Syncer) {
-	for {
-		select {
-		case sig := <-c:
-			fmt.Println(sig.String())
-			c := syncer.SavedConfig{}
-			for _, pair := range s.SyncPairs {
-				fmt.Println(pair.Left.Uri())
-				c.Pairs = append(c.Pairs, syncer.SyncPairConfig{
-					Left:   pair.Left.Uri(),
-					Right:  pair.Right.Uri(),
-					Config: pair.Config,
-				})
-				b, _ := json.Marshal(c.Pairs)
-				ioutil.WriteFile("config.json", b, 0777)
-				os.Exit(0)
-			}
-		}
+func NewPair(s *syncer.Syncer, r *http.Request) string {
+	err := r.ParseForm()
+	if err != nil {
+		return err.Error()
 	}
+
+	lName := r.FormValue("left")
+	rName := r.FormValue("right")
+	err = s.NewPair(syncer.SyncConfig{true, true, true}, lName, rName)
+	if err != nil {
+		return err.Error()
+	}
+	return lName + " ==> " + rName
+
 }
